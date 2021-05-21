b906a154496b932ceb586bf955b3f2a83843abe4
ahmad-ali14 <aallii300300@gmail.com>
2021-05-21 01:58:25 +0100

    journal 6

--- DirectoryList.java
+++ DirectoryList.java
@@ -2,40 +2,63 @@ import java.io.File;
 import java.util.Scanner;
 
 /**
- * This program lists the files in a directory specified by
- * the user.  The user is asked to type in a directory name.
- * If the name entered by the user is not a directory, a
- * message is printed and the program ends.
+ * This program lists the files in a directory specified by the user. The user
+ * is asked to type in a directory name. If the name entered by the user is not
+ * a directory, a message is printed and the program ends.
  */
 public class DirectoryList {
 
+   String directoryName; // Directory name entered by the user.
+   File directory; // File object referring to the directory.
+   String[] files; // Array of file names in the directory.
+   static Scanner scanner; // For reading a line of input from the user.
+
+   DirectoryList(String fileName) {
+      this.directoryName = fileName;
+   }
 
    public static void main(String[] args) {
-
-      String directoryName;  // Directory name entered by the user.
-      File directory;        // File object referring to the directory.
-      String[] files;        // Array of file names in the directory.
-      Scanner scanner;       // For reading a line of input from the user.
-
-      scanner = new Scanner(System.in);  // scanner reads from standard input.
-
+      scanner = new Scanner(System.in); // scanner reads from standard input.
       System.out.print("Enter a directory name: ");
-      directoryName = scanner.nextLine().trim();
-      directory = new File(directoryName);
-
-      if (directory.isDirectory() == false) {
-         if (directory.exists() == false)
-            System.out.println("There is no such directory!");
-         else
-            System.out.println("That file is not a directory.");
-      }
-      else {
-         files = directory.list();
-         System.out.println("Files in directory \"" + directory + "\":");
-         for (int i = 0; i < files.length; i++)
-            System.out.println("   " + files[i]);
-      }
+      String input = scanner.nextLine().trim();
+      DirectoryList dl = new DirectoryList(input);
+      dl.walk();
 
    } // end main()
+
+   private void walk() {
+      this.directory = new File(this.directoryName);
+
+      if (this.directory.isDirectory() == false) {
+         if (this.directory.exists() == false) {
+            /**
+             * first base case, where the recursion function should return if it reaches a dead end, or dead start.
+             * this case is useful when you start with a non existent directory.
+             */
+            System.out.println("There is no such directory! " + this.directory);
+            return;
+         } else {
+            /**
+             * The second base case, if the current directory is a file, just print the file name then return to upper level flow.
+             */
+            System.out.println("   " + this.directoryName);
+            return;
+         }
+      } else {
+         /**
+          * Recursion body, if you face a directory, list all files in this directory, then walk() each of them separately.
+          */
+         this.files = directory.list();
+         System.out.println("Files in directory \"" + directory + "\":");
+         for (String f : this.files) {
+            /**
+             * it's important, to append the current directoryName to each of the files name, so you can get the relative path to the file/directory.
+             * otherwise it will be considered as absolute path, and it's very rare that your file resides in the /home directory.
+             */
+            DirectoryList dl = new DirectoryList(this.directoryName + "/" + f);
+            dl.walk();
+         }
+      }
+   }
 
 } // end class DirectoryList
